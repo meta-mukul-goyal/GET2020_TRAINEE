7fd22a6062e43ff25488296018cf8828e38b20ac
mukul goyal <mukul.goyal@metacube.com>
2020-03-02 14:24:17 +0530

    assignment 9 done

---
+++ SCF/Session9/Assignment9/src/graphicsLib/OriginDistComparator.java
@@ -0,0 +1,19 @@
+package graphicsLib;
+
+import java.util.Comparator;
+
+/**
+ * This class implements comparator to sort objects based on origin-distance.
+ * @author Martand
+ *
+ */
+public class OriginDistComparator implements Comparator<Shape> {
+	public int compare(Shape s1,Shape s2){  
+		if(s1.getOrigin()==s2.getOrigin())  
+			return 0;  
+		else if(s1.getOrigin()>s2.getOrigin())  
+			return 1;  
+		else
+			return -1;  
+	}
+}
